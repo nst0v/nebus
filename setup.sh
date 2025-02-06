128da88bf3f7dbbdd7ae2b5c45a6e623e98b4f9d
dev-nms <dev-nms@modul.org>
2025-02-06 16:41:40 +0300

    fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 composer install
 cp .env.example .env
+chmod -R 775 storage bootstrap/cache
+sudo chown -R $USER:$USER storage bootstrap/cache
+./vendor/bin/sail down --volumes
 ./vendor/bin/sail up -d
 ./vendor/bin/sail artisan key:generate
+./vendor/bin/sail artisan storage:link
 ./vendor/bin/sail artisan migrate:fresh --seed
 ./vendor/bin/sail artisan l5-swagger:generate
-
+./vendor/bin/sail artisan optimize:clear
+./vendor/bin/sail artisan config:clear
+./vendor/bin/sail artisan cache:clear
